632212ffe0590a7347d7f15e452dcf36e8c393e9
Stephen Allard <stephencallard@gmail.com>
2020-04-28 04:21:19 -0500

    Added 'possible_moves', 'all_moves', and 'create_cell' methods

--- lib/knight_path.rb
+++ lib/knight_path.rb
@@ -2,11 +2,38 @@ class KnightPath
   attr_reader :source_position
 
   def initialize(input=[0,0])
-    @source_position = input
+    @source = create_cell(input)
   end
 
   public
 
-  
+  def possible_moves
+    #given a position, return an array of possible moves
+    #1. Given the source position, spit out all 8 moves 
+    #2. Check board and remove invalid positions
+    #3. Return result
+  end
+
+
+  def all_moves(position)
+    delta_x = [1,2,2,1,-1,-2,-2,1]
+    delta_y = [2,1,-1,-2,-2,-1,1,2]
+    result = []
+    i = 0
+
+    while i < x.length
+      result << []
+      i += 1
+    end
+
+  end
+
+  private
+
+  def create_cell(input)
+    Cell.new({
+      "position" => input
+    })
+  end
 
 end
