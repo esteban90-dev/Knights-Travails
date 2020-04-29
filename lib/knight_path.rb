f93a559df7fc69c5ac6942d9f495e1c3dc85f75e
Stephen Allard <stephencallard@gmail.com>
2020-04-29 07:37:27 -0500

    Changed all variable/attribute names to use 'cell' instead of 'position'

--- lib/knight_path.rb
+++ lib/knight_path.rb
@@ -1,50 +1,52 @@
 class KnightPath
-  attr_reader :start_position, :board
+  attr_reader :start_cell, :board
 
   def initialize(args)
-    @start_position = create_cell(args)
+    @start_cell = create_cell(args)
     @board = args.fetch("board")
   end
 
   public
 
-  def possible_moves(position)
-    #given a position, return an array of possible moves
-    all_moves(position).select{ |move| board.contains?(move) }
+  def possible_moves(cell)
+    #given a cell, return an array of possible moves
+    all_moves(cell).select{ |move| board.contains?(move) }
   end
 
-  def shortest_path(end_position)
+  def shortest_path(end_cell)
     #returns shortest path from start to end
     queue = []
-    visited_positions = []
-    queue << start_position
+    visited_cells = []
+    queue << start_cell
     
     while queue.length > 0
       puts "queue length: #{queue.length}"
-      #dequeue and see if current_position is the destination
-      current_position = queue.shift
-      if current_position.position == end_position
-        return path(start_position, current_position)
+      #dequeue
+      current_cell = queue.shift
+      #visited_cells << current_cell.position
+
+      if current_cell.position == end_cell
+        return path(start_cell, current_cell)
       else
-        #set current_position's neighbors to all the next available moves
-        current_position.neighbors = possible_moves(current_position.position).map do |move|
-          create_cell({ "previous" => current_position, "position" => move })
+        #set current_cell's neighbors to all the next available moves
+        current_cell.neighbors = possible_moves(current_cell.position).map do |move|
+          create_cell({ "previous" => current_cell, "position" => move })
         end
 
         #enqueue the neighbors
-        current_position.neighbors.each{ |neighbor| queue << neighbor }
+        current_cell.neighbors.each{ |neighbor| queue << neighbor }
       end
     end
   end
 
-  def all_moves(position)
-    #returns all possible cell coordinates a knight can move to from its current position in one move
+  def all_moves(cell)
+    #returns all possible cell coordinates a knight can move to from its current cell in one move
     delta_x = [1,2,2,1,-1,-2,-2,-1]
     delta_y = [2,1,-1,-2,-2,-1,1,2]
     result = []
     i = 0
     while i < delta_x.length
-      result << [ position[0] + delta_x[i], position[1] + delta_y[i] ]
+      result << [ cell[0] + delta_x[i], cell[1] + delta_y[i] ]
       i += 1
     end
     result
