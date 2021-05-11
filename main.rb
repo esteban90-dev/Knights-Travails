ec81309b186851e9bb187ca4e606e70fa3c62e86
Stephen Allard <stephencallard@gmail.com>
2021-05-11 10:39:23 -0500

    prompt user for starting/ending position and display the # of total moves

--- main.rb
+++ main.rb
@@ -10,14 +10,30 @@ def knight_moves(start_position, end_position)
   raise "Invalid start position given board size" unless board1.contains?(start_position)
   raise "Invalid end position given board size" unless board1.contains?(end_position)
 
+  puts "finding the shortest path between #{start_position} and #{end_position}...."
+
   trip1 = KnightPath.new({
     "position" => start_position,
     "board" => board1
   })
 
-  trip1.shortest_path(end_position).each{ |point| puts "#{point}" }
+  path = trip1.shortest_path(end_position)
+  path.each{ |point| puts "#{point}" }
+  puts "the knight made the trip in #{path.length - 1} moves"
 end
 
-knight_moves([0,0],[1,0])
+puts "Enter the starting x position of the knight (between 0 and 7)"
+start_position_x = gets.chomp.to_i
+
+puts "Enter the starting y position of the knight (between 0 and 7)"
+start_position_y = gets.chomp.to_i
+
+puts "Enter the ending x position of the knight (between 0 and 7)"
+end_position_x = gets.chomp.to_i
+
+puts "Enter the ending y position of the knight (between 0 and 7)"
+end_position_y = gets.chomp.to_i
+
+knight_moves([start_position_x, start_position_y], [end_position_x, end_position_y])
 
 
